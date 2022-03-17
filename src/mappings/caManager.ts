2df0e61ce745f4300e5934024174b634ce5adc56
Jaemin Kim <ddevkim@gmail.com>
2022-03-17 23:44:04 +0900

    feat/caManager

--- src/mappings/caManager.ts
+++ src/mappings/caManager.ts
@@ -3,13 +3,50 @@ import {
   ManagerContractRemoved,
   NftContractRegistered,
 } from '../types/OmnuumCAManager/OmnuumCAManager';
-import { NftContract } from '../types/schema';
-import { saveTransaction } from '../utils';
+import { Contract } from '../types/schema';
+import { convertContractTopicToString, saveTransaction } from '../utils';
 
 export function handleNftContractRegistered(event: NftContractRegistered): void {
-  const id = event.params.nftContract;
+  const id = event.params.nftContract.toHexString();
 
-  let nftRegisterEntity = NftContract.load(id);
+  let contractEntity = Contract.load(id);
+  if (!contractEntity) {
+    contractEntity = new Contract(id);
+  }
+  const transaction = saveTransaction(event);
+
+  contractEntity.id = id;
+  contractEntity.register_transaction = transaction.id;
+  contractEntity.owner = event.params.nftOwner;
+  contractEntity.topic = 'NFT';
+  contractEntity.is_removed = false;
+  contractEntity.save();
 }
-export function handleContractRegistered(event: ManagerContractRegistered): void {}
-export function handleContractRemoved(event: ManagerContractRemoved): void {}
+export function handleContractRegistered(event: ManagerContractRegistered): void {
+  const id = event.params.managerContract.toHexString();
+
+  let contractEntity = Contract.load(id);
+  if (!contractEntity) {
+    contractEntity = new Contract(id);
+  }
+  const transaction = saveTransaction(event);
+
+  contractEntity.id = id;
+  contractEntity.register_transaction = transaction.id;
+  contractEntity.owner = event.transaction.from;
+  contractEntity.topic = convertContractTopicToString(event.params.topic);
+  contractEntity.is_removed = false;
+  contractEntity.save();
+}
+export function handleContractRemoved(event: ManagerContractRemoved): void {
+  const id = event.params.managerContract.toHexString();
+
+  let contractEntity = Contract.load(id);
+  const transaction = saveTransaction(event);
+
+  if (contractEntity) {
+    contractEntity.is_removed = true;
+    contractEntity.remove_transaction = transaction.id;
+    contractEntity.save();
+  }
+}
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,19 +1,9 @@
-import { Entity, ethereum, log } from '@graphprotocol/graph-ts';
+import { crypto, Entity, ethereum, log } from '@graphprotocol/graph-ts';
 import { Approval, Transaction } from '../types/schema';
 import { Address, BigInt } from '@graphprotocol/graph-ts/common/numbers';
-import { Bytes } from '@graphprotocol/graph-ts/common/collections';
+import { ByteArray, Bytes } from '@graphprotocol/graph-ts/common/collections';
 
 export function saveTransaction(event: ethereum.Event): Transaction {
-  log.debug('_____TRANSACTION {}', [event.transaction.hash.toHexString()]);
-  log.debug('_______TRANSACTION {} {} {} {} {} {} {}', [
-    event.transaction.from.toHexString(),
-    event.transaction.to!.toHexString(),
-    event.block.gasUsed.toString(),
-    event.transaction.value.toString(),
-    event.block.number.toString(),
-    event.block.timestamp.toString(),
-    event.transaction.gasPrice.toString(),
-  ]);
   const id: string = event.transaction.hash.toHexString();
   let transaction = Transaction.load(event.transaction.hash.toHexString());
   if (!transaction) {
@@ -37,4 +27,24 @@ export function getUniqueIdFromTxLog(event: ethereum.Event): string {
   const trxHash = event.transaction.hash.toHexString();
   const logIndex = event.logIndex.toHexString();
   return `${trxHash}_${logIndex}`;
+}
+
+export function convertContractTopicToString(topicHash: Bytes): string {
+  if (crypto.keccak256(ByteArray.fromUTF8('NFT'))) {
+    return 'NFT';
+  } else if (crypto.keccak256(ByteArray.fromUTF8('VERIFIER'))) {
+    return 'VERIFIER';
+  } else if (crypto.keccak256(ByteArray.fromUTF8('TICKET'))) {
+    return 'TICKET';
+  } else if (crypto.keccak256(ByteArray.fromUTF8('MINTMANAGER'))) {
+    return 'MINTMANAGER';
+  } else if (crypto.keccak256(ByteArray.fromUTF8('EXCHANGE'))) {
+    return 'EXCHANGE';
+  } else if (crypto.keccak256(ByteArray.fromUTF8('WALLET'))) {
+    return 'WALLET';
+  } else if (crypto.keccak256(ByteArray.fromUTF8('REVEAL'))) {
+    return 'REVEAL';
+  } else {
+    return 'UNRECOGNIZED';
+  }
 }
